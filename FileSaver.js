29fda953f3966a6e7e598c6a37f3df41539d2f5e
Yassine Bousseta <133719472+devbousseta@users.noreply.github.com>
2023-05-17 22:41:59 +0000

    update

---
+++ FileSaver.js
@@ -0,0 +1,91 @@
+
+
+/*--- Donwload file txt ---*/
+
+function download() {
+    var text = document.getElementById("my-textarea").value;
+    var title = document.getElementById("title").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain" });
+    var anchor = document.createElement("a");
+    if(title != ''){
+        anchor.download = title + ".txt";
+    }else{
+        anchor.download = "File.txt";
+    }
+    
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+
+  // choose path ===
+//   async function download() {
+//     var text = document.getElementById("my-textarea").value;
+//     var title = document.getElementById("title").value;
+//     text = text.replace(/\n/g, "\r\n"); // To retain the line breaks.
+//     const fileHandle = await window.showSaveFilePicker({
+//       suggestedName: title + ".txt",
+//       types: [{
+//         description: 'Text Files',
+//         accept: {
+//           'text/plain': ['.txt']
+//         }
+//       }]
+//     });
+//     const writableStream = await fileHandle.createWritable();
+//     await writableStream.write(text);
+//     await writableStream.close();
+//   }
+  
+
+/*--- Change Direction  ---*/
+
+ $("body").on('input', 'textarea', function() {
+
+  
+
+      var el  = $(this);
+
+      var len = el.val().length;
+
+  
+
+      if (len <= 1){
+
+        var x = new RegExp("[A-Za-z]"); // is ascii
+
+        var isAscii = x.test(el.val().substring(0, 1));
+
+        if(isAscii){
+
+            el.css("direction", "ltr");
+
+        } else {
+
+            el.css("direction", "rtl");
+
+        }
+
+    }
+
+  
+
+});
+
+/*--- Clear text area  ---*/
+
+var input = document.querySelector('#clear');
+
+var textarea = document.querySelector('#my-textarea');
+
+
+
+input.addEventListener('click', function () {
+
+    textarea.value = '';
+
+}, false);
+
--- ckeditorST/config.js
+++
@@ -1,6 +0,0 @@
-CKEDITOR.editorConfig = function( config ) {
-	config.language = 'en';
-	config.uiColor = '#dae9f4';
-	config.height = 400;
-	config.toolbarCanCollapse = true;
-};
