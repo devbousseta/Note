83971d7d5ec02053d6871d9f1d85042bacc65dc7
Yassine Bousseta <133719472+devbousseta@users.noreply.github.com>
2023-05-19 08:57:53 +0000

    apdated

--- FileSaver.js
+++ FileSaver.js
@@ -4,12 +4,18 @@
 
 function download() {
     var text = document.getElementById("my-textarea").value;
+    var extention = document.getElementById("extention").value;
     var title = document.getElementById("title").value;
     text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
     var blob = new Blob([text], { type: "text/plain" });
     var anchor = document.createElement("a");
     if(title != ''){
-        anchor.download = title + ".txt";
+      if(extention !=''){
+          anchor.download = title+"."+extention;
+      }else{
+           anchor.download = title + ".txt";
+      }
+      
     }else{
         anchor.download = "File.txt";
     }
@@ -53,6 +59,73 @@ function download() {
         html2pdf().from(container).save("File.pdf");
     }
     
+  }
+
+
+    //download pdf //
+function creaPdf() {
+    // Long text
+    var titolo = document.querySelector('#input-fileName').value;
+    var text = document.querySelector('#textarea').value;
+    var repeat = parseInt(document.querySelector('#repeat').value) || 0;
+    for (var i=0; i<repeat; i++) 
+      text += text;
+  
+    // Create doc
+    var ori = document.querySelector('#ori').checked ? "portrait":"landscape";
+    var doc = new jsPDF(ori, 'mm', 'a4');
+    doc.setFontSize(parseInt(document.querySelector('#fontsize').value) || 20)
+  
+    // Page size
+    var pageSize = { h: doc.internal.pageSize.height, w: doc.internal.pageSize.width };
+    var margin = {top:20,left:15,bottom:15,right:15};
+    var lineHeight = doc.getTextDimensions("M").h / 2.54;// in to mm
+    console.log("SIZE: ", pageSize, lineHeight);
+    
+    // Pages handler
+    var linePos=0, nbPage=0;
+    function addPage(doc) {
+      if (nbPage) doc.addPage();
+      else nbPage = 0;
+      nbPage++;
+      linePos = margin.top;
+      // Header
+      // var title = document.querySelector('#input-fileName').value;
+      // var twidth = doc.getTextDimensions(title).w / 2.54;
+      // doc.text(pageSize.w/2 -twidth/2, margin.top, title);
+      // doc.rect(margin.left, margin.top -lineHeight, 
+      //          pageSize.w -margin.left - margin.right, 
+      //          1.5*lineHeight)
+      // Footer
+      var title = document.querySelector('#input-fileName').value;
+      var numPage = 'page '+nbPage;
+      var nwidth = doc.getTextDimensions(numPage).w / 2.54;
+      doc.text(pageSize.w -margin.right -nwidth, 
+               pageSize.h -margin.bottom -lineHeight, 
+               numPage);
+      doc.line(margin.left, pageSize.h -margin.bottom -2*lineHeight, 
+               pageSize.w - margin.right, 
+               pageSize.h -margin.bottom -2*lineHeight)
+      //
+      linePos = margin.top + 2*lineHeight;
+      return nbPage;
+    }
+    function addLine(doc, text) {
+      doc.text(margin.left, linePos, text);
+      linePos += lineHeight;  
+      if (linePos > pageSize.h-2*margin.bottom) {
+        addPage(doc);
+      }
+    }
+  
+    // Split text to page width
+    text = doc.splitTextToSize(text,pageSize.w-margin.left-margin.right);
+  
+    addPage(doc);
+    for (var i=0; i<text.length; i++){  
+      addLine(doc, text[i]);
+    }
+    doc.save(titolo + '.pdf')
   }
   
   
